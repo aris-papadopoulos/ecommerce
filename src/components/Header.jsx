261957707cda409884d66e28959bed9cb245f1c6
Aris Papadopoulos <aris.ppd@gmail.com>
2020-04-25 18:21:46 +0300

    Header: Adds link to homepage on logo click, body scroll visible
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
 import '../styles/header.scss';
 
 function App() {
   return (
         <header className="header">
-            <img src={logo} className="logo" alt="logo" />
+            <Link to={'/'}>
+                <img src={logo} className="logo" alt="logo" />
+            </Link>
         </header>
   );
 }
