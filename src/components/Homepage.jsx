c1b563368f3e78340a2dd399ac561cbf4bff86c5
Aris Papadopoulos <aris.ppd@gmail.com>
2020-04-25 14:04:26 +0300

    Homepage: Connects it to store, fetches categories on mount
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { listCategories } from '../actions';
 import logo from '../logo.svg';
 import '../styles/app.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Homepage = (props) => {
+
+    const { categories, listCategories } = props;
+
+    useEffect(() => {
+        if (!categories.length) {
+            listCategories();
+        }
+    }, [categories, listCategories]);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
-export default App;
+function mapStateToProps(state) {
+    return {
+        categories: state.categories
+    }
+}
+
+export default connect(mapStateToProps, { listCategories })(Homepage);
