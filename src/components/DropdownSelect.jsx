7eacfaee96d135adbeff5cc7d69bed47fd46f034
Aris Papadopoulos <aris.ppd@gmail.com>
2020-04-26 20:38:11 +0300

    Toolbar: Minor styling changes
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

--- src/components/DropdownSelect.jsx
+++ src/components/DropdownSelect.jsx
@@ -16,7 +16,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
     formControl: {
       margin: theme.spacing(1),
-      minWidth: 120,
+      minWidth: 90,
     },
     selectEmpty: {
       marginTop: theme.spacing(2),
