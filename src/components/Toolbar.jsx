baff8b7a7f1c127a6019b751045df2feefe27326
Aris Papadopoulos <aris.ppd@gmail.com>
2020-04-26 17:18:09 +0300

    Adds toolbar component for holding sorting/filtering subcomponents
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PriceRange from './PriceRange';
+import DropdownSelect from './DropdownSelect';
+
+const Toolbar = (props) => {
+
+    const { category } = props;
+
+    const sortBy = {
+        id: 'sortBy', 
+        label: 'Ταξινόμηση κατά',
+        labelWidth: 120,
+        options: [{
+            label: 'Τιμή',
+            value: 'price'
+        },
+        {
+            label: 'Τίτλος',
+            value: 'title'
+        }]
+    };
+    const orderBy = {
+        id: 'order', 
+        label: 'Σειρά', 
+        labelWidth: 40,
+        options: [{
+            label: 'Αύξουσα',
+            value: 'asc'
+        },
+        {
+            label: 'Φθήνουσα',
+            value: 'desc'
+        }]
+    };
+
+    return (
+        <div className="category-options">
+            <PriceRange category={category} />
+            <DropdownSelect values={sortBy} />
+            <DropdownSelect values={orderBy} />
+        </div>
+    );
+}
+
+export default connect(null, null)(Toolbar);
