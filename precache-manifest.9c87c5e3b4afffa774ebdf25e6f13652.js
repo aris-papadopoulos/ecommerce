d91c54b951b64eb9777e4fbb9ea5d9d9489024fc
Aris Papadopoulos <aris.ppd@gmail.com>
2020-05-28 18:23:38 +0300

    Updates

---
+++ precache-manifest.9c87c5e3b4afffa774ebdf25e6f13652.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "024a30b1c13e1b8de3fc4789e18daea0",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "c5d47d64fe46966eec5e",
+    "url": "/ecommerce/static/css/main.38dffa8f.chunk.css"
+  },
+  {
+    "revision": "907279cb4ae7eb5a0596",
+    "url": "/ecommerce/static/js/2.821967e5.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/ecommerce/static/js/2.821967e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5d47d64fe46966eec5e",
+    "url": "/ecommerce/static/js/main.822b85a6.chunk.js"
+  },
+  {
+    "revision": "52bbb57f47ced24c6851",
+    "url": "/ecommerce/static/js/runtime-main.984cd6c4.js"
+  },
+  {
+    "revision": "2440514b5268e66c18ade286d2e28a57",
+    "url": "/ecommerce/static/media/logo.2440514b.svg"
+  }
+]);
